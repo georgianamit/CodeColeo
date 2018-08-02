2653d6a10c8ff08d636c1196a3e59ad57ad0b1da
Amit Rawat <georgianamitrawat@gmail.com>
2018-08-02 23:21:06 +0530

    worked on post list ajax

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,13 +1,25 @@
 from rest_framework import serializers
 from posts.models import Post
 from accounts.api.serializers import UserDisplaySerializer
+from django.utils.timesince import timesince
 
 
 class PostModelSerializer(serializers.ModelSerializer):
     user = UserDisplaySerializer(read_only=True)
+    date_display = serializers.SerializerMethodField()
+    timesince = serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields =[
             'user',
-            'content'
+            'content',
+            'timestamp',
+            'date_display',
+            'timesince'
         ]
+    
+    def get_date_display(self,obj):
+        return obj.timestamp.strftime("%b %d %I:%M %p")
+
+    def get_timesince(self, obj):
+        return timesince(obj.timestamp) + " ago"
--- src/static-storage/scripts/post_list.js
+++ src/static-storage/scripts/post_list.js
@@ -14,42 +14,51 @@ $(document).ready(function () {
     var query = getParameterByName('q')
     var postList = [];
 
+    function attachPost(postValue, prepend) {
+        var postContent = postValue.content;
+        var postUser = postValue.user;
+        var timeSince = postValue.timesince;
+        var postHtmlBody = '<div class="card card-small card-post mb-4" >'+
+            '<div class="card-body" >'+
+                '<div class="card-post__author d-flex">'+
+                    '<a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url(\'images/avatars/1.jpg\');">Written by James Khan</a>'+
+                    '<div class="d-flex flex-column justify-content-center ml-3">'+
+                        '<span class="card-post__author-name">' + '@' + postUser.username + '</span>'+
+                        '<small class="text-muted">'+ timeSince +'</small>'+
+                    '</div>'+
+                '</div> <hr>'+
+                    '<!-- <h5 class="card-title">{{ object.title }}</h5> -->'+
+'<p class="card-text text-muted">' + postContent + '</p>'+
+        '</div>'+
+                '<div class="card-footer border-top d-flex">'+
+                    '<div class="my-auto ml-2">'+
+                        '<a class="btn btn-sm btn-white" href="#"> Like <span class="badge badge-pill badge-primary">22323</span></a>'+
+                    '</div>'+
+                    '<div class="my-auto ml-2">'+
+                        '<a class="btn btn-sm btn-white" href="#"> Dislike <span class="badge badge-pill badge-danger">22323</span> </a>'+
+                    '</div>'+
+                    '<div class="my-auto ml-2">'+
+                        '<a class="btn btn-sm btn-white" href="#"> Share <span class="badge badge-pill badge-success">22323</span> </a>'+
+                    '</div>'+
+                    '<div class="my-auto ml-auto">'+
+                        '<a class="btn btn-sm btn-white" href="#"> Read more </a>'+
+                    '</div>'+
+                '</div>'+
+                '</div>';
+        if(prepend == true){
+            $("#post-list").prepend(postHtmlBody);
+        }else{
+            $("#post-list").append(postHtmlBody);
+        }
+    }
+
     function parsePosts() {
         if(postList == 0){
             $("#post-list").text("No Posts Found!")
         }else{
             $.each(postList, function (key, value) {
                 var postKey = key;
-                var postContent = value.content;
-                var postUser = value.user;
-                var postBody = '<div class="card card-small card-post mb-4" >'+
-                    '<div class="card-body" >'+
-                        '<div class="card-post__author d-flex">'+
-                            '<a href="#" class="card-post__author-avatar card-post__author-avatar--small" style="background-image: url(\'images/avatars/1.jpg\');">Written by James Khan</a>'+
-                            '<div class="d-flex flex-column justify-content-center ml-3">'+
-                                '<span class="card-post__author-name">' + '@' + postUser.username + '</span>'+
-                                '<small class="text-muted">{{ object.timestamp }}</small>'+
-                            '</div>'+
-                        '</div> <hr>'+
-                            '<!-- <h5 class="card-title">{{ object.title }}</h5> -->'+
-    '<p class="card-text text-muted">' + postContent + '</p>'+
-                '</div>'+
-                        '<div class="card-footer border-top d-flex">'+
-                            '<div class="my-auto ml-2">'+
-                                '<a class="btn btn-sm btn-white" href="#"> Like <span class="badge badge-pill badge-primary">22323</span></a>'+
-                            '</div>'+
-                            '<div class="my-auto ml-2">'+
-                                '<a class="btn btn-sm btn-white" href="#"> Dislike <span class="badge badge-pill badge-danger">22323</span> </a>'+
-                            '</div>'+
-                            '<div class="my-auto ml-2">'+
-                                '<a class="btn btn-sm btn-white" href="#"> Share <span class="badge badge-pill badge-success">22323</span> </a>'+
-                            '</div>'+
-                            '<div class="my-auto ml-auto">'+
-                                '<a class="btn btn-sm btn-white" href="#"> Read more </a>'+
-                            '</div>'+
-                        '</div>'+
-                        '</div>';
-                        $("#post-list").append(postBody);
+                attachPost(value,true)
             })
         }
 
@@ -77,6 +86,9 @@ $(document).ready(function () {
     fetchPosts()
 
     $("#post-form").submit(function(event){
+        event.preventDefault()
+        console.log('preventing...')
+        console.log(event)
         var this_ = $(this)
         var formData = this_.serialize()
 
@@ -85,8 +97,7 @@ $(document).ready(function () {
             method: 'POST',
             data: formData,
             success: function (data) {
-                console.log(data)
-                fetchPosts()
+                attachPost(data,true);
             },
             error: function (data) {
                 console.log("error")
