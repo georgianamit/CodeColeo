1780ddc00e517aadf0aacd6e3d100b7b8da68d3e
Amit Rawat <georgianamitrawat@gmail.com>
2018-08-09 23:02:39 +0530

    worked on profile page

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,9 +3,28 @@ from django.conf import settings
 
 # Create your models here.
 
+class UserProfileManager(models.Manager):
+    use_for_related_fields = True
+
+    def all(self):
+        qs = self.get_queryset().all()
+
+        try:
+            if self.instance:
+                qs = qs.exclude(user=self.instance)
+        except:
+            pass
+        return qs
+
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='profile')
     following = models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True, related_name='followed_by')
+
+    objects= UserProfileManager()
+
+    def get_following(self):
+        users = self.following.all()
+        return users.exclude(username = self.user.username)
 
     def __str__(self):
         return str(self.following.all().count())
