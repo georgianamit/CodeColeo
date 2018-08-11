c16e76250c681b00ab325944128786f8a374c2df
Amit Rawat <georgianamitrawat@gmail.com>
2018-08-11 23:57:02 +0530

    worked on follow and unfollow button

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
-from .views import UserDetailView
+from .views import UserDetailView, UserFollowView
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url='/')),
     # url(r'^create/$', PostCreateView.as_view(), name='create'),
+    
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
+
     # url(r'^update/(?P<pk>\d+)/$', PostUpdateView.as_view(), name='update'),
     # url(r'^delete/(?P<pk>\d+)/$', PostDeleteView.as_view(), name='delete'),
     # url(r'^list/', PostListView.as_view(), name='list'),
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import DetailView
+from django.views import View
 from django.contrib.auth.models import User
+from .models import UserProfile
 # Create your views here.
 
 class UserDetailView(DetailView):
@@ -12,6 +14,18 @@ class UserDetailView(DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        following = UserProfile.objects.is_following(self.request.user,self.get_object())
         context["sidebar"] = False
+        context["following"] = following
         return context
+    
+
+
+class UserFollowView(View):
+
+    def get(self, request, username, *args, **kwargs):
+        toggle_user = get_object_or_404(User, username__iexact=username)
+        if request.user.is_authenticated():
+            is_following = UserProfile.objects.toggle_follow(request.user, toggle_user)
+        return redirect("profiles:detail", username=username)
     
