f6412bd7280a281a5b111d045b9c2ec68f99f52e
Amit Rawat <georgianamitrawat@gmail.com>
2018-09-20 23:10:15 +0530

    made search page

--- src/posts/api/urls.py
+++ src/posts/api/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
-from .views import PostListAPIView, PostCreateAPIView, ShareAPIView, LikeToggleAPIView
+from .views import PostListAPIView, PostCreateAPIView, ShareAPIView, LikeToggleAPIView, PostDetailAPIView
 
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url='/')),
     url(r'^create/$', PostCreateAPIView.as_view(), name='create'),
+    # url(r'^search/$', SearchAPIView.as_view(), name='api-search'),
     url(r'^share/(?P<pk>\d+)/$', ShareAPIView.as_view(), name='share'),
     url(r'^like/(?P<pk>\d+)/$', LikeToggleAPIView.as_view(), name='like-toggle'),
-    # url(r'^detail/(?P<pk>\d+)/$', PostDetailView.as_view(), name='Detail'),
+    url(r'^detail/(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='Detail'),
     # url(r'^update/(?P<pk>\d+)/$', PostUpdateView.as_view(), name='Update'),
     # url(r'^delete/(?P<pk>\d+)/$', PostDeleteView.as_view(), name='Delete'),
     url(r'^$', PostListAPIView.as_view(), name='List'),
